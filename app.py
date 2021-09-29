0f20046cbb4f53ccce681ebafb6fe7bf01990001
S.K.Roshan <63838060+dev-Roshan-lab@users.noreply.github.com>
2021-09-29 15:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request
 import pandas as pd
-import csv
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "home"
+    return """
+    <h1>Hey There 👋</h1>
+    <p>head over to <a href="/api/v1?url=sample">Endpoint</a> to view a sample of the API and replace sample with a raw CSV link to get your production ready Backend, already configured for you</p>
+    """
 
 @app.route('/api/v1')
 def api():
@@ -20,10 +21,14 @@ def api():
             message = "url not found"
         )
     
-    df = pd.read_csv(url)
-    data = df.to_dict('records')
-    print(data)
-    return jsonify(data)
+    if (url == 'sample'):
+        df = pd.read_csv('https://raw.githubusercontent.com/dev-Roshan-lab/csv2api/main/sample.csv?token=APHBO3A3CRITP2ZMRKMOIBDBLV2D4')
+        data = df.to_dict('records')
+        return jsonify(data)
+    else:
+        df = pd.read_csv(url)
+        data = df.to_dict('records')
+        return jsonify(data)
 
 
 if __name__ == "__main__":
